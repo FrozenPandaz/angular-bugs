7f42d18e89594d5daa40e9f37f8890720666120d
Jason Jean <jasonjean1993@gmail.com>
2018-10-03 22:42:09 -0400

    repro Element not defined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
   providers: [],
+  entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const element = createCustomElement(AppComponent, {
+      injector: this.injector
+    });
+    customElements.define('app-root', element);
+  }
+}
